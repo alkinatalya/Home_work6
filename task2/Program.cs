fce0cae56453d84c0819015bad099bfc5cc101d8
Natalya <alkinatalya@yandex.com>
2022-10-03 08:38:45 +0400

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string strValue;
+    strValue = Console.ReadLine() ?? "0";
+    int value = int.Parse(strValue);
+    return value;
+}
+int firstValue = Prompt("Введите первое число >");
+int secondValue = Prompt("Введите второе число >");
+int thirdValue = Prompt("Введите третье число >");
+
+int result = firstValue;
+
+if (secondValue > result)
+{
+    result = secondValue;
+}
+if (thirdValue > result)
+{
+    result = thirdValue;
+}
+
+System.Console.Write($" {result},");
+
+ 
